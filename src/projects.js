b36e592c955c22b9e399abfa06f0b0873e640429
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-28 15:31:54 -0800

    Add list of all projects

---
+++ src/projects.js
@@ -0,0 +1,70 @@
+const projects = [
+    {
+        name: "Command Line Interface Themed Portfolio",
+        repoUrl: "",
+        deployUrl: "https://bytesizeerror.github.io/",
+        img: "https://image.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg",
+    },
+    {
+        name: "Microsoft's DCCD Charitable Givings App Prototype",
+        repoUrl: "https://github.com/p10dibb/TeamMicrosoft423",
+        deployUrl: "",
+        img: "http://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4OAgf?ver=6a31",
+    },
+    {
+        name: "Music Trivia 2.0",
+        repoUrl: "https://github.com/ChonaC/Project-2-MUSICTRIVIA",
+        deployUrl: "https://protected-falls-91774.herokuapp.com/",
+        img: "https://i.postimg.cc/SQMSzcHD/music-trivia-logo.png",
+    },
+    {
+        name: "The Tech Blog",
+        repoUrl: "https://github.com/ByteSizeError/Tech-Blog",
+        deployUrl: "https://glacial-ridge-40516.herokuapp.com/",
+        img: "https://image.freepik.com/free-vector/organic-flat-blog-post-illustration-with-people_23-2148955260.jpg",
+    },
+    {
+        name: "Weather Dashboard",
+        repoUrl: "https://github.com/ByteSizeError/Weather-Dashboard",
+        deployUrl: "https://bytesizeerror.github.io/Weather-Dashboard/",
+        img: "https://image.freepik.com/free-vector/meteorology-abstract-concept-vector-illustration-met-station-meteorology-program-university-degree-weather-prediction-method-measuring-instruments-atmosphere-study-abstract-metaphor_335657-1980.jpg",
+    },
+    {
+        name: "Code Quiz",
+        repoUrl: "https://github.com/ByteSizeError/Code-Quiz",
+        deployUrl: "https://bytesizeerror.github.io/Code-Quiz/",
+        img: "https://image.freepik.com/free-vector/quiz-neon-sign_1262-19629.jpg",
+    },
+    {
+        name: "Note Taker",
+        repoUrl: "https://github.com/ByteSizeError/Note-Taker",
+        deployUrl: "https://whispering-taiga-97956.herokuapp.com/",
+        img: "https://image.freepik.com/free-vector/office-school-stationery-background_1284-58335.jpg",
+    },
+    {
+        name: "E-Commerce Back End",
+        repoUrl: "https://github.com/ByteSizeError/E-Commerce-Back-End",
+        deployUrl: "",
+        img: "https://image.freepik.com/free-vector/isometric-laptop-with-shopping-cart-keypad_1262-16544.jpg",
+    },
+    {
+        name: "Employee Management System",
+        repoUrl: "https://github.com/ByteSizeError/Employee-Management-System",
+        deployUrl: "",
+        img: "https://image.freepik.com/free-vector/choosing-best-candidate-concept_52683-43377.jpg",
+    },
+    {
+        name: "Team Profile Generator",
+        repoUrl: "https://github.com/ByteSizeError/Team-Profile-Generator",
+        deployUrl: "",
+        img: "https://image.freepik.com/free-vector/recruiting-professionals-studying-candidate-profiles_1262-21404.jpg",
+    },
+    {
+        name: "Password Generator",
+        repoUrl: "https://github.com/ByteSizeError/Password-Generator",
+        deployUrl: "https://bytesizeerror.github.io/Password-Generator/",
+        img: "https://image.freepik.com/free-vector/biometric-security-concept_53876-90466.jpg",
+    },
+];
+
+export default projects;
