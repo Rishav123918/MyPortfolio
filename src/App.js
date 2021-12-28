93dd8183a46178ef24377886d0e122c8373f6a80
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-28 15:32:24 -0800

    Update to include all sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+import projects from "./projects";
 
 function App() {
-    return <Navbar />;
+    return (
+        <main>
+            <Navbar />
+            <AboutMe />
+            <Projects projects={projects} />
+            <Contact />
+        </main>
+    );
 }
 
 export default App;
