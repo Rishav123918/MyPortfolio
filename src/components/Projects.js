3edcfa45786ba63602ebf4a6a466854668d75197
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-30 15:54:06 -0800

    Add icons from font awesome

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import projects from "../projects";
 import Project from "./Project";
 
 function Projects(props) {
@@ -14,7 +13,8 @@ function Projects(props) {
                 className="
                         section-body
                         projects
-                        row row-cols-1 row-cols-lg-2
+                        row row-cols-1 
+                        row-cols-lg-2
                         g-4
                         ms-1
                         me-1
