b66a18cc2b1979ec70c4627a3dc824406586a49f
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-30 20:05:28 -0800

    Add code to make links show

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,7 +13,28 @@ function Project(props) {
                 <div className="card-body">
                     <h5 className="card-title">{props.project.name}</h5>
                 </div>
-                <div className="card-footer"></div>
+                <div className="card-footer">
+                    {props.project.repoUrl !== "" ? (
+                        <a
+                            className="btn btn-outline-primary"
+                            href={props.project.repoUrl}
+                        >
+                            Repository
+                        </a>
+                    ) : (
+                        ""
+                    )}
+                    {props.project.deployUrl !== "" ? (
+                        <a
+                            className="btn btn-outline-primary"
+                            href={props.project.deployUrl}
+                        >
+                            Deployed Link
+                        </a>
+                    ) : (
+                        ""
+                    )}
+                </div>
             </div>
         </div>
     );
