edcc9db7130d9f554abd876672de82c7c781c5a7
Jimmy Zheng <jimmyz0622@gmail.com>
2021-12-30 19:50:09 -0800

    Add name to center of landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,14 @@ import React from "react";
 import "../styles/Landing.css";
 
 function Landing() {
-    return <div id="landing" className="scroll-snap"></div>;
+    return (
+        <div id="landing" className="scroll-snap">
+            <div className="center">
+                <h1>Jimmy Zheng</h1>
+                <h5>Software Developer</h5>
+            </div>
+        </div>
+    );
 }
 
 export default Landing;
