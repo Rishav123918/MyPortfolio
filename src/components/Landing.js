b5c9909833ba9385038e01afcf633d1925831591
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-01 20:55:08 -0800

    Add typing animation for name

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../styles/Landing.css";
+import TypeAnimation from "react-type-animation";
 
 function Landing() {
     return (
         <div id="landing" className="scroll-snap">
             <div className="center">
-                <h1>Jimmy Zheng</h1>
+                <TypeAnimation
+                    cursor={true}
+                    sequence={["", 500, "Jimmy Zheng", 2000]}
+                    wrapper="h1"
+                    repeat={Infinity}
+                />
                 <h5>Software Developer</h5>
             </div>
         </div>
