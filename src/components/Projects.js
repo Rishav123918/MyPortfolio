42a6ee34cf7cc75b6a86fdad937132a1dd60e89b
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-02 11:36:13 -0800

    Add scrolling for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Project from "./Project";
+import "../styles/Projects.css";
 
 function Projects(props) {
     return (
         <section id="projects" className="scroll-snap">
             <header className="section-header">
-                <h2>Projects</h2>
+                <h2>Portfolio</h2>
             </header>
             <div className="section-line"></div>
             <div
-                id="projects"
+                id="scroll-projects"
                 className="
                         section-body
-                        projects
-                        row row-cols-1 
-                        row-cols-lg-2
                         g-4
                         ms-1
                         me-1
