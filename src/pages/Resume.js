49d053eb805121ae5df1de03ba31606b779cf78f
Jimmy Zheng <jimmyz0622@gmail.com>
2022-08-11 19:43:29 -0700

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Skill from "../components/Skill";
-import resume from "../assets/Jimmy-Zheng-Resume.pdf";
+import resume from "../assets/JimmyZhengResume.pdf";
 
 function Resume(props) {
     const handlePageChange = (e) => {
