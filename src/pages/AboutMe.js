a190ae7132483c7bce2fe8abe38faadd895600c9
Jimmy Zheng <jimmyz0622@gmail.com>
2023-07-10 22:50:56 -0700

    Update about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -18,31 +18,58 @@ function AboutMe(props) {
             </header>
             <div className="section-body ms-3 mb-3">
                 <p>
-                    Full Stack Developer experienced in making iOS and web
-                    applications with strengths in meeting deadlines,
-                    creativity, organization, and problem solving. Graduated
-                    from Washington State University with a degree in Computer
-                    Science and am currently pursuing a Certificate in Full
-                    Stack Development from the University of Washington.
+                    Hello! I'm Jimmy Zheng, a passionate software engineer with
+                    a focus on creating innovative solutions that drive user
+                    engagement and improve overall system performance. With a
+                    strong background in full-stack development and a knack for
+                    problem-solving, I strive to deliver high-quality products
+                    that exceed user expectations.
                 </p>
                 <p>
-                    Throughout my time at Washington State University, I worked
-                    hard, remained organized to ensure that I met all my
-                    deadlines. I relied heavily on my time management skills and
-                    stayed calm when under pressure. Ultimately, it paid off. I
-                    completed my bachelor's degree and graduated{" "}
-                    <i>magna cum laude</i>. From my numerous team projects I
-                    learned the importance of teamwork and communication. The
-                    whole process showed me the benefits of hard work, teamwork,
-                    and dedication with the rewards that followed.
+                    During my time at Medici, I developed an NFT generator using
+                    TypeScript, allowing users to create and customize thousands
+                    of tokens simultaneously. By collaborating closely with the
+                    design team, I ensured a visually appealing and
+                    user-friendly UI, resulting in a remarkable 35% boost in
+                    user engagement. I also integrated web3 functionality,
+                    enabling seamless wallet connection and artwork minting.
                 </p>
                 <p>
-                    Known for being an adaptive problem solver excited on
-                    developing apps. Passionate about tackling problems from
-                    different angles and breaking problems down piece by piece.
-                    In each project, my goal is to create an impactful user
-                    experience with responsive applications by leveraging my
-                    skills.
+                    As a Software Engineer and Mentor at SCALE Pathways, I
+                    improved front-end usability by implementing React and Ant
+                    Design, leading to a significant 20% increase in user
+                    engagement. By leveraging technologies such as Node.js and
+                    Express.js, I enhanced overall performance and
+                    functionality, reducing the average response time by 30%.
+                    Additionally, I mentored senior design students, providing
+                    guidance and support in their development.
+                </p>
+                <p>
+                    At Mr. Sushi, I created an efficient ordering system for
+                    tabletop kiosks and QR scans, resulting in a 30% increase in
+                    restaurant efficiency. By collaborating closely with
+                    restaurant staff, I optimized the system based on
+                    operational requirements, achieving a seamless adoption rate
+                    of 95% among the staff.
+                </p>
+                <p>
+                    With a Certificate in Full-Stack Development from the
+                    University of Washington and a Bachelor's degree in Computer
+                    Science <i>(magna cum laude)</i> from Washington State
+                    University, I bring a solid foundation and a drive for
+                    excellence to every project I undertake. I'm also the proud
+                    recipient of awards such as the STARS Scholarship, EECS
+                    Scholarship, and 3rd Place in Senior Design Competition.
+                </p>
+                <p>
+                    Please take a look at my React Portfolio to explore some of
+                    the projects I've worked on and see my skills in action.
+                </p>
+                <p>
+                    I'm passionate about leveraging my skills and experience to
+                    create impactful software solutions. If you have a project
+                    or an opportunity you'd like to discuss, I'd love to hear
+                    from you. Let's connect and make a difference together!
                 </p>
             </div>
             <div className="mt-auto scroll-snap-end">
